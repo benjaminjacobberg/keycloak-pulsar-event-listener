b8cefca95b160006a5f40f91754eae812b2c884c
Benjamin Berg <benjaminjacobberg@gmail.com>
2022-08-23 07:49:15 -0400

    Removing event formatting

--- src/main/java/com/github/benjaminjacobberg/PulsarEventListenerProvider.java
+++ src/main/java/com/github/benjaminjacobberg/PulsarEventListenerProvider.java
@@ -6,9 +6,6 @@ import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.admin.AdminEvent;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 public class PulsarEventListenerProvider implements EventListenerProvider {
 
     private final Producer<byte[]> producer;
@@ -22,17 +19,7 @@ public class PulsarEventListenerProvider implements EventListenerProvider {
     @Override
     public void onEvent(Event event) {
         try {
-            com.github.benjaminjacobberg.Event<Event> pulsarEvent = new com.github.benjaminjacobberg.Event<>(
-                    UUID.randomUUID(),
-                    event.getUserId(),
-                    "IdentityProvider",
-                    1,
-                    event,
-                    event.getType().toString(),
-                    LocalDateTime.now()
-            );
-            String body = objectMapper.writeValueAsString(pulsarEvent);
-            producer.send(body.getBytes());
+            producer.send(objectMapper.writeValueAsString(event).getBytes());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -41,17 +28,7 @@ public class PulsarEventListenerProvider implements EventListenerProvider {
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
         try {
-            com.github.benjaminjacobberg.Event<AdminEvent> pulsarEvent = new com.github.benjaminjacobberg.Event<>(
-                    UUID.randomUUID(),
-                    adminEvent.getRealmId(),
-                    "IdentityProvider",
-                    1,
-                    adminEvent,
-                    adminEvent.getOperationType().toString() + "_" + adminEvent.getResourceTypeAsString(),
-                    LocalDateTime.now()
-            );
-            String body = objectMapper.writeValueAsString(pulsarEvent);
-            producer.send(body.getBytes());
+            producer.send(objectMapper.writeValueAsString(adminEvent).getBytes());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
